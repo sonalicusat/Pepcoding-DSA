2ab7545f95a504df456d941d2d4fbd3058bed586
Sonali Choudhary <62690758+sonalicusat@users.noreply.github.com>
2022-01-24 21:35:41 +0530

    Create Print Z.java

---
+++ Print Z.java
@@ -0,0 +1,30 @@
+/*
+1. You are required to print a 'z' of size 5 using '*'.                               
+
+Constraints
+No Constraints
+
+Format
+Input
+There is no input
+
+Example
+Sample Output
+*****
+   *
+  *
+ *
+***** 
+*/
+
+//Solution 1:
+
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Write your code here
+        System.out.print("*****\n   *\n  *\n *\n*****");
+    }
+}
