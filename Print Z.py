1cdb322f316939bc0f4bed9759acd50dff641396
Sonali Choudhary <62690758+sonalicusat@users.noreply.github.com>
2022-01-24 21:36:48 +0530

    Create Print Z.py

---
+++ Print Z.py
@@ -0,0 +1,19 @@
+"""
+ 1. You are required to print a 'z' of size 5 using '*'.                               
+
+Constraints
+No Constraints
+
+Format
+Input
+There is no input
+
+Sample Output
+*****
+   *
+  *
+ *
+*****
+"""
+
+print("*****\n   *\n  *\n *\n*****")
