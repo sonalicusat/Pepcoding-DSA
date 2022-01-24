0398890eabf51055644687ab24c0b6d2303efe94
Sonali Choudhary <62690758+sonalicusat@users.noreply.github.com>
2022-01-24 21:40:24 +0530

    Create Grading System.py

---
+++ Grading System.py
@@ -0,0 +1,42 @@
+"""
+1. You are given as input marks of a student.
+2. Display an appropriate message based on the following rules:
+  	  2.1 for marks above 90, print excellent.
+  	  2.2 for marks above 80 and less than equal to 90, print good.
+  	  2.3 for marks above 70 and less than equal to 80, print fair.
+  	  2.4 for marks above 60 and less than equal to 70, print meets expectations.
+  	  2.5 for marks less than equal to 60, print below par.
+
+Note -> Only change the code in area after - // code here
+
+Constraints
+No constraints
+
+Format
+Input
+Input is handled for you and provided as variable marks
+
+Output
+Appropriate message as per student's marks
+
+Example
+Sample Input
+
+92
+
+Sample Output
+excellent
+"""
+
+marks=int(input())
+
+if marks > 90:
+    print("excellent")
+elif marks > 80:
+    print("good")
+elif marks > 70:
+    print("fair")
+elif marks > 60:
+    print("meets expectations")
+else:
+    print("below par")
